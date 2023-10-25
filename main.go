43a8240d07f5606fbc5d4bd6a32572567d7e347e
vipul-rawat-zs <vipul.rawat@zopsmart.com>
2023-10-25 11:50:32 +0530

    Add redis datastore

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import "gofr.dev/pkg/gofr"
+import (
+	"gofr.dev/pkg/gofr"
+)
 
 func main() {
 	app := gofr.New()
 
 	app.GET("/", func(ctx *gofr.Context) (interface{}, error) {
-		return "Hello gofr!", nil
+		var resp string
+
+		err := ctx.Redis.Get(ctx, "test").Scan(&resp)
+		if err != nil {
+			return nil, err
+		}
+
+		return resp, nil
 	})
 
 	app.Start()
